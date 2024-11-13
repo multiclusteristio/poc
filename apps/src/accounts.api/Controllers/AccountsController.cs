9b1eedd290e5d444ecf0d2ef4b5a67214aaebdd7
Turkel <gadirzade@gmail.com>
2024-11-13 22:06:25 +0400

    account vs canary enabled

--- apps/src/accounts.api/Controllers/AccountsController.cs
+++ apps/src/accounts.api/Controllers/AccountsController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Accounts.API.Controllers
 {
     [ApiVersion("1.0")]
+    [ApiVersion( "2.0" )]
     [Route("api/v{v:apiVersion}")]
     [ApiController]
     public class AccountsController : ControllerBase
@@ -30,6 +31,26 @@ namespace Accounts.API.Controllers
                 BranchCode = "9142"
             });
         }
+
+        [HttpGet("accounts")]
+        [MapToApiVersion( "2.0" )]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(Account), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Error[]), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(Error[]), StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(typeof(Error[]), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetAccountV2([FromQuery] string accountNumber)
+        {
+            return Ok(new Account
+            {
+                AccountNumber = accountNumber,
+                AccountType = "Loan",
+                CIF = "1",
+                Balance = 10,
+                BranchCode = "9142"
+            });
+        }        
 
         [HttpPost("accounts")]
         [Consumes(MediaTypeNames.Application.Json)]
--- apps/src/accounts.api/docker.sh
+++ apps/src/accounts.api/docker.sh
@@ -1,1 +1,1 @@
-sh build_and_push.sh account v2.3
+sh build_and_push.sh account v2.4
