20dc493682bc675d9da855fc614fb992b347b168
Turkel <gadirzade@gmail.com>
2024-11-13 21:13:33 +0400

    sasa

--- apps/src/accounts.api/Controllers/AccountsController.cs
+++ apps/src/accounts.api/Controllers/AccountsController.cs
@@ -44,28 +44,5 @@ namespace Accounts.API.Controllers
         }
     }
 
-    [ApiVersion("2.0")]
-    [Route("api/accounts/v{v:apiVersion}")]
-    [ApiController]
-    public class AccountsControllerV2 : ControllerBase
-    {
-        [HttpGet("accounts")]
-        [Consumes(MediaTypeNames.Application.Json)]
-        [Produces(MediaTypeNames.Application.Json)]
-        [ProducesResponseType(typeof(Account), StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(Error[]), StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(typeof(Error[]), StatusCodes.Status401Unauthorized)]
-        [ProducesResponseType(typeof(Error[]), StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> GetAccount([FromQuery] string accountNumber)
-        {
-            return Ok(new Account
-            {
-                AccountNumber = accountNumber,
-                AccountType = "Loan V2",
-                CIF = "1",
-                Balance = 10,
-                BranchCode = "9142"
-            });
-        }
-    }
+
 }
