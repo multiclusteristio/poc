0fdbc3881bb337c713672624641a209e4f5fee52
Turkel <gadirzade@gmail.com>
2024-11-13 20:02:19 +0400

    accounts vs edit

--- apps/src/accounts.api/Controllers/AccountsController.cs
+++ apps/src/accounts.api/Controllers/AccountsController.cs
@@ -7,12 +7,12 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Accounts.API.Controllers
 {
-    [ApiVersion("1.0")]
-    [Route("api/v{v:apiVersion}")]
+    [Route("api/accounts")]
     [ApiController]
     public class AccountsController : ControllerBase
     {
-        [HttpGet("accounts")]
+        // Version 1.0 - Get Account
+        [HttpGet("v1/accounts")]
         [Consumes(MediaTypeNames.Application.Json)]
         [Produces(MediaTypeNames.Application.Json)]
         [ProducesResponseType(typeof(Account), StatusCodes.Status200OK)]
@@ -24,14 +24,15 @@ namespace Accounts.API.Controllers
             return Ok(new Account
             {
                 AccountNumber = accountNumber,
-                AccountType = "Loan",
+                AccountType = "Loan V1",
                 CIF = "1",
                 Balance = 10,
                 BranchCode = "9142"
             });
         }
 
-        [HttpPost("accounts")]
+        // Version 1.0 - Create Account
+        [HttpPost("v1/accounts")]
         [Consumes(MediaTypeNames.Application.Json)]
         [Produces(MediaTypeNames.Application.Json)]
         [ProducesResponseType(StatusCodes.Status201Created)]
@@ -41,6 +42,26 @@ namespace Accounts.API.Controllers
         public async Task<IActionResult> Create([FromBody] NewAccountRequest request)
         {
             return Created($"accounts/{Guid.NewGuid()}", null);
+        }
+
+        // Version 2.0 - Get Account
+        [HttpGet("v2/accounts")]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(Account), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Error[]), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(Error[]), StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(typeof(Error[]), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetAccountV2([FromQuery] string accountNumber)
+        {
+            return Ok(new Account
+            {
+                AccountNumber = accountNumber,
+                AccountType = "Loan V2",
+                CIF = "1",
+                Balance = 10,
+                BranchCode = "9142"
+            });
         }
     }
 }
